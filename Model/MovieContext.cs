e4a7e2f8e15f7f47eae9cbfd6bf59c85d82dc441
Manjula.Kamma <Manjula.Kamma@SOLNB-5HX9F94>
2025-07-04 09:17:27 +0530

    Add project files.

---
+++ Model/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication3.Model
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+        public DbSet<Movies> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Update connection string as per your database
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=ManjuDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
+
---
+++ Model/Movies.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Model
+{
+    public class Movies
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? MovieName { get; set; }
+
+        [Required]
+        public string? DirectorName { get; set; }
+
+        [Required]
+        public int YearOfRelease { get; set; }
+    }
+}
